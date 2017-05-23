491aced930efe6ac5aec4391cc7aa42c939b9446
Alex <alex333sh@gmail.com>
2017-05-23 18:27:39 -0400

    Initial commit

---
+++ getip.ps1
@@ -0,0 +1,63 @@
+$logFile = "E:\ip_output.txt"
+
+Function GetIP {
+	$ip = (Invoke-WebRequest -Uri https://api.ipify.org -DisableKeepAlive).Content	
+	return $ip
+}
+
+Function KillProcess {
+	Stop-Process -processname qbittorrent* -force
+	Stop-Process -processname pia_nw* -force
+	Stop-Process -processname rubyw* -force
+	Stop-Process -processname pia_manager* -force
+}
+
+Function StartVPN {
+	$vpn = "C:\Program Files\pia_manager\pia_manager.exe"
+	start-process $vpn
+	do {
+		Log "Waiting for VPN to start..."
+		Start-Sleep -s 5
+	} while ($(IsVPNRunning) -ne $true)
+}
+
+Function StartBT {
+	$bt = "C:\Program Files (x86)\qBittorrent\qbittorrent.exe"
+	start-process $bt
+}
+
+Function Wait {
+	for ($a=0; $a -lt 20; $a++) {
+			Write-Host -NoNewline "."
+			Start-Sleep -s 1
+		}
+}
+
+Function IsVPNRunning {
+	$vpn_process = Get-Process pia_manager -ErrorAction SilentlyContinue
+	if ($vpn_process) {
+		return $true
+	}
+	return $false
+}
+
+Function Log {
+   Param ([string]$logstring)
+   Add-content $Logfile -value $logstring
+   Write-Host $logstring
+}
+
+Function LogFileOnly {
+   Param ([string]$logstring)
+   Add-content $Logfile -value $logstring
+}
+
+Function LogProcessKillInfo {
+	Param ([string]$timestamp)
+
+	Log ""
+	Log "-------------------------"
+	Log " *** IP changed to local"
+	Log " current time: $timestamp"
+	Log "-------------------------"
+}
---
+++ ip_check.ps1
@@ -0,0 +1,67 @@
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+}
+
+. "C:\Users\alex_media\Desktop\getip.ps1"
+
+$ip_local = ""
+$script:ip_previous = $null
+
+Function SetPreviousIP {
+	Param ([string]$ip_current)
+	
+	if ($ip_previous -eq $null) {
+		$script:ip_previous = $ip_current
+	}
+	elseif ($ip_previous -ne $ip_current) {
+		Log "*** IP changed from $ip_previous ***"
+		$script:ip_previous = $ip_current
+		Log "*** setting previous IP to $ip_previous ***"
+	}
+	else {}
+}
+
+
+While ($true) {
+	$timestamp = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+	$ip_current = GetIP
+	SetPreviousIP $ip_current
+	
+	Write-Host -NoNewline $timestamp $ip_current
+	Write-Host -NoNewline " "
+
+	LogFileOnly "$timestamp --- $ip_current"
+	
+	if ($ip_current -eq $ip_local) {
+		LogProcessKillInfo $timestamp
+		Log "*** VPN app is running: $(IsVPNRunning)"
+		KillProcess 
+		Log "*** attempting to start VPN"
+		StartVPN
+		$start_counter = 1
+		Log "*** checking for VPN connection"
+		while ($ip_current -eq $ip_local) {
+		    Log "current ip:  $ip_current"
+			if ($start_counter -eq 10) {
+				Log "Tried 10 times, restarting VPN again"
+				KillProcess
+				StartVPN
+				$start_counter = 1
+			}
+			$ip_current = GetIP
+			LogFileOnly "attemp : $start_counter "
+			Write-Host -NoNewline "attemp : $start_counter "
+			Wait
+			""
+			$start_counter++
+		}
+		Log "*** starting BT"
+		StartBT
+	}
+	else {
+		Wait
+	}
+	Write-Host ""
+}
